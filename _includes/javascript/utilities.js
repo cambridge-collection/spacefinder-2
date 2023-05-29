61cdfe801ec970f7f565b08a6b85a908f1f7c0dd
Michael Hawkins <mjh39@cam.ac.uk>
2023-05-29 11:09:46 +0100

    Fix misnamed parameter

--- _includes/javascript/utilities.js
+++ _includes/javascript/utilities.js
@@ -93,7 +93,7 @@ function haversine_distance( mk1, mk2 ) {
  * @param {Object} options Information about the JSON file
  * @param {String} options.key Unique key used to store the data in localstorage (required)
  * @param {String} options.url URL of the JSON file (required)
- * @param {Integer} options.expiry How long to cache the results (in hours) default: 24
+ * @param {Integer} options.expires How long to cache the results (in hours) default: 24
  * @param {Function} options.callback callback function with one parameter (JSON parsed response)
  */
 function getJSON( options ) {
