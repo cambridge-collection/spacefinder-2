f04fb30d63fc6fbe241eed87f9386fe5d385f889
Michael J Hawkins <mjh39@cam.ac.uk>
2023-06-15 11:36:16 +0100

    Update slug field to ensure it always conforms to the title (#25)

--- _scripts/concat.js
+++ _scripts/concat.js
@@ -4,15 +4,19 @@
  */
 const fs = require('fs');
 const path = require('path');
-const spacefiles = fs.readdirSync( path.resolve( __dirname, '../spaces' ), { encoding: 'utf8' } );
+const spacefiles = fs.readdirSync( path.resolve( __dirname, '../spaces' ), { encoding: 'utf8' } )
+    .filter(file => {
+        return path.extname(file).toLowerCase() === '.json';
+    });
 const allSpaces = [];
 spacefiles.forEach( filename => {
     if ( filename !== '.' && filename !== '..' ) {
-        var data = fs.readFileSync( path.resolve( __dirname, '../spaces/', filename ) );
-        var jsondata = JSON.parse( data );
+        let data = fs.readFileSync( path.resolve( __dirname, '../spaces/', filename ), { encoding: 'utf8' } );
+        let jsondata = JSON.parse( data );
         if ( jsondata.published ) {
-            var geodata = JSON.parse( jsondata.location );
-            if ( geodata && geodata.coordinates && geodata.coordinates.length == 2 ) {
+            jsondata.slug = string_to_slug(jsondata.title);
+            let geodata = JSON.parse( jsondata.location );
+            if ( geodata && geodata.coordinates && geodata.coordinates.length === 2 ) {
                 jsondata.lat = geodata.coordinates[1];
                 jsondata.lng = geodata.coordinates[0];
             } else {
@@ -25,5 +29,20 @@ spacefiles.forEach( filename => {
 });
 fs.writeFileSync( path.resolve( __dirname, '../spaces.json' ), JSON.stringify( allSpaces ) );
 
+function string_to_slug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
 
+    // remove accents, swap ñ for n, etc
+    const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    const to = "aaaaeeeeiiiioooouuuunc------";
+    for (let i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
 
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
